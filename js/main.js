a8355939752a66065c47f380b1682501d943e822
Guldana Mehdiyeva <guldanamehdiyeva@gmail.com>
2021-02-12 18:02:49 +0400

    practise work 2

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*Tooltip===============================*/
+$('.insta').tipso({
+    content  : 'Instagram',
+    animationIn  : 'flipInX',
+    animationOut : 'flipOutX',
+    background : '#F9CF00',
+	color : 'black',
+	width : 120,
+});
+
+$('.twitter').tipso({
+    content  : 'Twitter',
+    animationIn  : 'flipInX',
+    animationOut : 'flipOutX',
+    background : '#F9CF00',
+	color : 'black',
+	width : 120,
+});
+
+/*Tooltip end =======================*/
+
+/*data animation*/
+AOS.init();
